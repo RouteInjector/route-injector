092a6ccbefe7094b2ca833225a301a3e81aa6563
Gerard Solé <g.sole.ca@gmail.com>
2016-11-10 15:37:48 +0100

    Exporting fsutils to injector.utils.fsutils

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,6 @@
 module.exports = require("./app/RouteInjector");
 module.exports.MongooseJsonform = require("./mongoose-plugins/mongoose-jsonform");
 module.exports.MongooseInjector = require("./mongoose-plugins/mongoose-injector");
+module.exports.utils = {
+    fsutils: require("./utils/FSUtils"),
+};
