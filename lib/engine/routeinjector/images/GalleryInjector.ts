8f3b072da29df43813f931e930843cbd23ec6de3
Alex Baena <a.baena@ondho.com>
2017-02-23 17:35:20 +0100

    allow gallery to send resized images

--- lib/engine/routeinjector/images/GalleryInjector.ts
+++ lib/engine/routeinjector/images/GalleryInjector.ts
@@ -100,6 +100,16 @@ class GalleryInjector {
 
     private handleGetImage() {
         let express = this.routeInjector.internals.express;
+            var IMGR = require('imgr').IMGR;
+            var imgr= new IMGR(this.routeInjector.config.env.images.imgrConfig || {});
+
+            imgr.serve(this.routeInjector.config.env.images.path) //folder
+                .namespace(this.prefix + this.galleryEndpoint)// /image
+                .cacheDir(this.routeInjector.config.env.images.cache)
+                .urlRewrite('/:path/:size/:file.:ext') // '/:path/:size/:file.:ext'
+                .using(this.routeInjector.app);
+
+
         this.routeInjector.app.use(this.prefix + this.galleryEndpoint, express.static(this.galleryFilepath));
     }
 
